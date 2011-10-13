4e3b8ee05b52c2c96b4900d7fb87c39346b90302
hnam1985 <hnam1985@gmail.com>
2011-10-13 11:34:48 +0000

    unfinished. Weapon Manager with buffered shot

--- Assets/Scripts/Weapon.js
+++ Assets/Scripts/Weapon.js
@@ -1,6 +1,8 @@
 //component for 'weapon' prefab
 class Weapon extends ScriptableObject{
 	public var needPosition:boolean = false;
+	public var cooldown:float = 0.5;
+	public var bulletPrefab: GameObject;
 	/*public function saySth(){
 		Debug.Log("abcdef");
 	}*/

44fd67b73b560c6854caa76c8550ff0cdc0ab25b
hnam1985 <hnam1985@gmail.com>
2011-10-23 16:13:36 +0000

    fix joystick control on multiplayer

--- Assets/Scripts/Weapons/Health.js
+++ Assets/Scripts/Weapons/Health.js
@@ -118,6 +118,7 @@ function OnEnable () {
 // Regenerate health
 
 function Regenerate () {
+	if (!networkView.isMine) return;	
 	if (regenerateSpeed > 0.0f) {
 		while (enabled) {
 			if (Time.time > lastDamageTime + 3) {

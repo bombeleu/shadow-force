ee6e2e35aab6d84ed7fa1b8908aeed1e779ff429
no7boy <no7boy@gmail.com>
2011-10-31 06:56:22 +0000

    prefab modified

--- Assets/Scripts/VisionMeshScript.js
+++ Assets/Scripts/VisionMeshScript.js
@@ -1,5 +1,6 @@
 #pragma strict
 
+
 private var _mesh : Mesh; // current vision mesh
 private var _newVertices : Vector3[];
 private var _tmpVertices : Vector3[];
@@ -144,10 +145,6 @@ private function Process( blocker : GameObject )
 		//Debug.Log("triangle : " + _newVertices[2*i+1].x + "," + _newVertices[2*i+1].z + "---" + _newVertices[2*i+2].x + "," +_newVertices[2*i+2].z);
 	}
 	
-	//Debug.Log(_tmpVertices[0] + ","+_tmpVertices[1] + ","+_tmpVertices[2]);
-	//Debug.Log(_tmpVertices[0] + ","+_tmpVertices[3] + ","+_tmpVertices[4]);
-	//Debug.Log(_tmpVertices[0] + ","+_tmpVertices[5] + ","+_tmpVertices[6]);
-
 	//Debug.Log("Triangle Count:" + _triangleCount);
 	//_newVertices = _tmpVertices;
 	for (i = 1 ; i <= 2*_triangleCount; i++)
@@ -173,7 +170,8 @@ private function GetDistance(blocker : GameObject) : float
 		return (distance > viewDistance) ? INFINITY : distance;
 	} else 
 	{
-		distance = (_playerTransform.position - blocker.transform.position).magnitude;
+		distance = (_playerTransform.position - blocker.transform.position).magnitude 
+					- Mathf.Max(blocker.transform.localScale.x,blocker.transform.localScale.z);
 		
 		return (distance > viewDistance) ? INFINITY : distance;
 	}
@@ -184,15 +182,15 @@ private function GetDistance(blocker : GameObject) : float
 private function GetAngleValue(v : Vector2) : float
 {
 	var sign : int;
-	var REGION_ANGLE : float = 10000;
+	var REGION_ANGLE : float = 1000;
 	// first, get sign
 	if (v.y < 0)
 	{
-		if (v.x < 0) return 0;
-		else return 3 * REGION_ANGLE;
+		if (v.x < 0) return v.x/v.y;
+		else return 3 * REGION_ANGLE - v.y/v.x ;
 	} else 
 	{
-		if (v.x < 0) return REGION_ANGLE + v.x/v.y;
+		if (v.x < 0) return REGION_ANGLE + -v.y/v.x;
 		else return 2*REGION_ANGLE + v.x/v.y;
 	}
 	//return ( sign * 360 + Vector2.Angle(v,Vector2.up));
@@ -202,9 +200,14 @@ private function GetAngleValue(v : Vector2) : float
 private function GetIntersectPts(pt0 : Vector2, pt1 : Vector2, pt2 : Vector2, blocker : GameObject)
 {
 	//var _result : Vector4 = Vector4.zero;
+	var leftMost : Vector2 ;
+	var rightMost : Vector2;
+	var temp : Vector3;
 
+	_result[0] = _result[1] = _result[2] = _result[3] = Vector2.zero;
 	if (BlockerManager.IsCylinder(blocker))
 	{
+		return _result;
 		// find the intersection of the triangle and the circle
 		/*
 		http://paulbourke.net/geometry/2circle/
@@ -213,7 +216,7 @@ private function GetIntersectPts(pt0 : Vector2, pt1 : Vector2, pt2 : Vector2, bl
 		//var point : Vector2 = Vector2(transform.position.x,transform.position.z);
 		var point : Vector2 = Vector2.zero;
 		//var center : Vector2 = Vector2(blocker.transform.position.x, blocker.transform.position.z);
-		var temp : Vector3 = _playerTransform.InverseTransformPoint(blocker.transform.position);
+		temp = _playerTransform.InverseTransformPoint(blocker.transform.position);
 		var center : Vector2 = Vector2(temp.x,temp.z);
 		var r0 : float = blocker.transform.localScale.x * 0.5;
 		
@@ -228,138 +231,176 @@ private function GetIntersectPts(pt0 : Vector2, pt1 : Vector2, pt2 : Vector2, bl
 		//x3 = x2 +- h ( y1 - y0 ) / d
 		//y3 = y2 -+ h ( x1 - x0 ) / d
 		var hd : float = h/r1;
-		var tangent1 : Vector2 = Vector2(p2.x + hd*(midPoint.y-center.y),p2.y - hd*(midPoint.x-center.x));
-		var tangent2 : Vector2 = Vector2(p2.x - hd*(midPoint.y-center.y),p2.y + hd*(midPoint.x-center.x));
+		leftMost  = Vector2(p2.x + hd*(midPoint.y-center.y),p2.y - hd*(midPoint.x-center.x));
+		rightMost = Vector2(p2.x - hd*(midPoint.y-center.y),p2.y + hd*(midPoint.x-center.x));
 		
-		//Debug.Log("Tangent :" + tangent1 + "---" + tangent2);
-		// now we got 2 intersection point, need to determine how to break it
-		// ip1 supposed to be on the left side, swap if not
-		if (Sign(tangent1,Vector2.zero,Vector2.up) < Sign(tangent2,Vector2.zero,Vector2.up))
-		{
-			temp = tangent1;tangent1 = tangent2; tangent2 = temp;
-			//Debug.Log("Come here1");
-		}
 	
-		if (IsPointInTri(tangent1,pt0,pt1,pt2))
-		{
-			//Debug.Log("Come here2");
-			_result[0] = LineIntersectLine(pt0,tangent1,pt1,pt2,true);
-			_result[1] = tangent1;
-		} else
-		{
-			_result[1] = LineIntersectCircle(pt0,pt1,center,r0);
-		}
-		
-		if (IsPointInTri(tangent2,pt0,pt1,pt2))
-		{
-			//Debug.Log("Come here3");
-			_result[2] = tangent2;
-			_result[3] = LineIntersectLine(pt0,tangent2,pt1,pt2,true);
-		} else
-		{
-			_result[2] = LineIntersectCircle(pt0,pt2,center,r0);
-		}
 	} else
 	{
-		//var w : float = blocker.transform.localScale.x;
-		//var h : float = blocker.transform.localScale.z;
 		var mat : Matrix4x4 = _playerTransform.worldToLocalMatrix * blocker.transform.localToWorldMatrix;
-		
-		
 		var size : float = 0.5;
 		// get the 4 _corners
 		temp = mat.MultiplyPoint3x4(Vector3(-size,0,-size));_corner[0]  = Vector2(temp.x,temp.z);
 		temp = mat.MultiplyPoint3x4(Vector3(-size,0,size));_corner[1]  = Vector2(temp.x,temp.z);
 		temp = mat.MultiplyPoint3x4(Vector3(size,0,size));_corner[2]  = Vector2(temp.x,temp.z);
 		temp = mat.MultiplyPoint3x4(Vector3(size,0,-size));_corner[3]  = Vector2(temp.x,temp.z);
-		var leftMost : int = 0;
-		var rightMost : int = 0;
 		
 		
+		var sign01 : float = Sign2(_corner[0],_corner[1]) ;
+		var sign12 : float = Sign2(_corner[1],_corner[2]) ;
+		var sign23 : float = Sign2(_corner[3],_corner[2]) ;
+		var sign30 : float = Sign2(_corner[0],_corner[3]) ;
+		
+		if (sign01 * sign23 > 0)
+		{
+			if (sign12 * sign30 > 0)
+			{
+				if (Mathf.Abs(sign01) < Mathf.Abs(sign23))
+				{
+					if (Mathf.Abs(sign12) < Mathf.Abs(sign30))
+					{
+						leftMost = _corner[0];rightMost = _corner[2];
+					} else
+					{
+						leftMost = _corner[1];rightMost = _corner[3];
+					}
+				} else
+				{
+					if (Mathf.Abs(sign12) < Mathf.Abs(sign30))
+					{
+						leftMost = _corner[1];rightMost = _corner[3];
+					} else
+					{
+						leftMost = _corner[2];rightMost = _corner[0];
+					}
+				}
+			
+			} else
+			{
+				if (Mathf.Abs(sign01) < Mathf.Abs(sign23))
+				{
+					leftMost = _corner[0];rightMost = _corner[1];
+				} else
+				{
+					leftMost = _corner[2];rightMost = _corner[3];
+				}
+			}
+		} else 
+		{
+			if (sign12 * sign30 < 0) Debug.Log("SOMETHING WRONG !!");	
+			if (Mathf.Abs(sign12) < Mathf.Abs(sign30))
+			{
+				leftMost = _corner[1];rightMost = _corner[2];
+			} else
+			{
+				leftMost = _corner[3];rightMost = _corner[0];
+			}
+		} 
+		/*
 		for (var i : int = 0; i < 4; i++)
 		{
 			_angleCorner[i] = GetAngleValue(_corner[i]);
-			_result[i] = Vector2.zero;
 		}
-		
+	
+		var min : int = 0;
+		var max : int = 0;
 		for (i = 1; i < 4; i++)
 		{
-			//if (_corner[i].x < _corner[leftMost].x) leftMost = i;
-			//if ((_corner[i].x / _corner[i].y) < (_corner[leftMost].x / _corner[leftMost].y)) leftMost = i;
-			//if ((_corner[i].x / _corner[i].y) > (_corner[rightMost].x / _corner[rightMost].y)) rightMost = i;
-			//if (Vector2.Angle(_corner[i],Vector2.up) < Vector2.Angle(_corner[leftMost],Vector2.up)) leftMost = i;
-			//if (Vector2.Angle(_corner[i],Vector2.up) > Vector2.Angle(_corner[rightMost],Vector2.up)) rightMost = i;
-			
-			if (_angleCorner[i] < _angleCorner[leftMost]) leftMost = i;
-			if (_angleCorner[i] > _angleCorner[rightMost]) rightMost = i;
+			if (_angleCorner[i] < _angleCorner[min]) min = i;
+			if (_angleCorner[i] > _angleCorner[max]) max = i;
 		}
-		//rightMost = (leftMost + 2)%4;
+		leftMost = _corner[min];rightMost = _corner[max];
+		*/
 		//Debug.Log("4 _corner:" + _corner[0] + " | "+ _corner[1] + " | "+ _corner[2] + " | "+ _corner[3]);
 		//Debug.Log("Left - right" + leftMost + "," + rightMost);
+		//Debug.Log(_angleCorner[0] + ","+_angleCorner[1]+","+_angleCorner[2]+","+_angleCorner[3]);
+		//Debug.Log(sign01 + ","+sign12+","+sign23+","+sign30);
+	}
+	
+	
+	// now we got 2 intersection point, need to determine how to break it
+	// ip1 supposed to be on the left side, swap if not
+	if (GetAngleValue(leftMost) > GetAngleValue(rightMost))
+	{
+		var temp2 : Vector2 = leftMost;leftMost = rightMost; rightMost = temp2;
+	}
 		
-		var intersectPt1 : Vector2 = LineIntersectLine(pt0,pt1,_corner[leftMost],_corner[rightMost],false);
-		var intersectPt2 : Vector2 = LineIntersectLine(pt0,pt2,_corner[leftMost],_corner[rightMost],false);
-		var intersectPt3 : Vector2 = LineIntersectLine(pt1,pt2,_corner[leftMost],_corner[rightMost],false);
-		//Debug.Log("Intersect Pts:" + intersectPt1 + " | " + intersectPt2 + "|" + intersectPt3);
-		if (intersectPt1 != Vector2.zero)
+	// we got the block line [leftMost,rightMost], now calculate the intersect point
+	var intersectPt1 : Vector2 = LineIntersectLine(pt0,pt1,leftMost,rightMost,false);
+	var intersectPt2 : Vector2 = LineIntersectLine(pt0,pt2,leftMost,rightMost,false);
+	var intersectPt3 : Vector2 = LineIntersectLine(pt1,pt2,leftMost,rightMost,false);
+	//Debug.Log("Intersect Pts:" + intersectPt1 + " | " + intersectPt2 + "|" + intersectPt3);
+	if (intersectPt1 != Vector2.zero)
+	{
+		_result[1] = intersectPt1;
+		if (intersectPt2 != Vector2.zero) // intersect 1,2
 		{
-			_result[1] = intersectPt1;
-			
-			if (intersectPt2 != Vector2.zero) // intersect 1,2
+			_result[2] = intersectPt2;
+		} else if (intersectPt3 != Vector2.zero) // intersect 1,3
+		{
+			_result[2] = intersectPt3;
+			_result[3] = intersectPt3;
+		} else // only intersect 1
+		{
+			if (IsPointInTri(rightMost,pt0,pt1,pt2))
 			{
-				_result[2] = intersectPt2;
-			} else if (intersectPt3 != Vector2.zero) // intersect 1,3
+				_result[2] = rightMost;
+				_result[3] = LineIntersectLine(pt0,rightMost,pt1,pt2,true);
+			} else
 			{
-				_result[2] = intersectPt3;
-				_result[3] = intersectPt3;
-			} else // only intersect 1
-			{
-				_result[2] = _corner[rightMost];
-				_result[3] = LineIntersectLine(pt0,_corner[rightMost],pt1,pt2,true);
+				_result[2] = leftMost;
+				_result[3] = LineIntersectLine(pt0,leftMost,pt1,pt2,true);
 			}
-		} else
+		}
+	} else
+	{
+		if (intersectPt2 != Vector2.zero)
 		{
-			if (intersectPt2 != Vector2.zero)
+			_result[2] = intersectPt2;
+			if (intersectPt3 != Vector2.zero) // intersect 2,3
 			{
-				_result[2] = intersectPt2;
-				if (intersectPt3 != Vector2.zero) // intersect 2,3
+				_result[0] = intersectPt3;
+				_result[1] = intersectPt3;
+			} else // intersect 2 only
+			{
+				if (IsPointInTri(leftMost,pt0,pt1,pt2))
+				{
+					_result[0] = LineIntersectLine(pt0,leftMost,pt1,pt2,true);
+					_result[1] = leftMost;
+				} else
+				{
+					_result[0] = LineIntersectLine(pt0,rightMost,pt1,pt2,true);
+					_result[1] = rightMost;
+				}
+			}
+		} else 
+		{
+			if (intersectPt3 != Vector2.zero) // intersect 3 only
+			{
+				if (IsPointInTri(leftMost,pt0,pt1,pt2)) // pt1 inside
+				{
+					_result[0] = LineIntersectLine(pt0,leftMost,pt1,pt2,true);
+					_result[1] = leftMost;
+					_result[2] = intersectPt3;
+					_result[3] = intersectPt3;	
+				} else // pt2 must be inside
 				{
 					_result[0] = intersectPt3;
 					_result[1] = intersectPt3;
-				} else // intersect 2 only
-				{
-					_result[0] = LineIntersectLine(pt0,_corner[leftMost],pt1,pt2,true);
-					_result[1] = _corner[leftMost];
+					_result[2] = rightMost;
+					_result[3] = LineIntersectLine(pt0,rightMost,pt1,pt2,true);
 				}
-			} else 
+			} else if (IsPointInTri(leftMost,pt0,pt1,pt2))// no intersections, both pts are inside triangle
 			{
-				if (intersectPt3 != Vector2.zero) // intersect 3 only
-				{
-					if (IsPointInTri(_corner[leftMost],pt0,pt1,pt2)) // pt1 inside
-					{
-						_result[0] = LineIntersectLine(pt0,_corner[leftMost],pt1,pt2,true);
-						_result[1] = _corner[leftMost];
-						_result[2] = intersectPt3;
-						_result[3] = intersectPt3;	
-					} else // pt2 must be inside
-					{
-						_result[0] = intersectPt3;
-						_result[1] = intersectPt3;
-						_result[2] = _corner[rightMost];
-						_result[3] = LineIntersectLine(pt0,_corner[rightMost],pt1,pt2,true);
-					}
-				} else if (IsPointInTri(_corner[leftMost],pt0,pt1,pt2))// no intersections, both pts are inside triangle
-				{
-					_result[0] = LineIntersectLine(pt0,_corner[leftMost],pt1,pt2,true);
-					_result[1] = _corner[leftMost];
-					_result[2] = _corner[rightMost];
-					_result[3] = LineIntersectLine(pt0,_corner[rightMost],pt1,pt2,true);
-				}
+				_result[0] = LineIntersectLine(pt0,leftMost,pt1,pt2,true);
+				_result[1] = leftMost;
+				_result[2] = rightMost;
+				_result[3] = LineIntersectLine(pt0,rightMost,pt1,pt2,true);
 			}
 		}
-
 	}
-	//Debug.Log("Result is" + _result);
+
+	//Debug.Log("Result is:" + _result[0]+"||"+_result[1]+"||"+_result[2]+"||"+_result[3]);
 	
 	return _result;
 }
@@ -416,10 +457,16 @@ private function LineIntersectCircle(p1 : Vector2, p2 : Vector2, center : Vector
 	return v1;
 }
 
+private function Sign2(p1 : Vector2, p2 : Vector2) : float
+{
+	return p2.y*p1.x - p2.x*p1.y;
+}
+
 private function Sign(p1 : Vector2, p2 :Vector2, p3 :Vector2) : float 
 {  
 	return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
+
 private function IsPointInTri(pt :Vector2, v1:Vector2, v2:Vector2, v3:Vector2) : boolean
 {  
 	var b1: boolean;  var b2: boolean;  var b3: boolean;  

c7a7f26cbcca212f103707effde64ab6aa481f88
hnam1985 <hnam1985@gmail.com>
2011-11-10 05:50:48 +0000

    fix bug on StickyCam initialization fix Smoke random rotation

--- Assets/Scripts/Observer.js
+++ Assets/Scripts/Observer.js
@@ -4,7 +4,6 @@ public var range: float = 15;
 public var angle: float = 90;
 
 private var enable: boolean = true;
-
 private var viMeshScript:VisionMeshScript;
 
 function Awake(){
@@ -22,10 +21,23 @@ function Update () {
 function SetEnable(b:boolean){
 	if (enable == b) return;
 	enable = b;
-	viMeshScript.enabled = b;
-	viMeshScript.GetComponent.<Renderer>().enabled = b;
+	var viComp:Visibility = GetComponent.<Visibility>();
+	if (viComp){
+		SetViMeshVisible(b & viComp.GetVisible());
+	}else{
+		SetViMeshVisible(b);
+	}
 }
 
 function GetEnable():boolean{
 	return enable;
+}
+
+function OnSetVisible(visi:boolean){
+	SetViMeshVisible(visi & enable);
+}
+
+function SetViMeshVisible(b:boolean){
+	viMeshScript.enabled = b;
+	viMeshScript.renderer.enabled = b;
 }

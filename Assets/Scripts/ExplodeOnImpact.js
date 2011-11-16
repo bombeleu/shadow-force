ecb5575864c097b22ebf47eaab164c3ccff39a1c
no7boy <no7boy@gmail.com>
2011-11-16 17:27:52 +0000

    some prefab effect

--- Assets/Scripts/ExplodeOnImpact.js
+++ Assets/Scripts/ExplodeOnImpact.js
@@ -2,11 +2,24 @@
 @script RequireComponent (Rigidbody)
 
 public var explosionPrefab : GameObject;
+public var timeTillDie : float = 0.5;
+private var _triggered : boolean = false;
+private var _impactPosition : Vector3;
 
 function Update () {
+	if (_triggered)
+	{
+		transform.position = _impactPosition;
+		timeTillDie = timeTillDie - Time.deltaTime;
+		if (timeTillDie < 0)
+		{
+			Spawner.Destroy (gameObject);
+		}
+	}
 }
 
 function OnCollisionEnter(collision : Collision) {
-	Spawner.Destroy (gameObject);
 	Spawner.Spawn (explosionPrefab, transform.position, Quaternion.identity/*transform.rotation*/);
+	_impactPosition = transform.position;
+	_triggered = true;
 }

7557e6d25e0aebced0f5c025c71247c469d9b32b
hnam1985@gmail.com <hnam1985@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2011-12-02 08:20:45 +0000

    simplify dodging code

--- trunk/Assets/Scripts/AI/GravityDodger.js
+++ trunk/Assets/Scripts/AI/GravityDodger.js
@@ -34,31 +34,11 @@ function Update () {
 	}
 }
 
-private var affecting : Hashtable = new Hashtable();
-
 function OnTriggerStay (other : Collider) : void{
 	var ai : DodgingAI = other.GetComponentInChildren.<DodgingAI>();
 	if (ai){
 		var dir:Vector3 = other.transform.position - landingPos;
 		dir.y = 0;
-		if (!affecting.ContainsKey(ai.gameObject.GetInstanceID()))
-			affecting.Add(ai.gameObject.GetInstanceID(), ai);
-		ai.OnEvadeZone(dir.normalized*(affectRadius-dir.magnitude), gameObject);
-	}
-}
-
-function OnTriggerExit (other : Collider) : void{
-	var ai : DodgingAI = other.GetComponentInChildren.<DodgingAI>();
-	if (ai){
-		if (affecting.ContainsKey(ai.gameObject.GetInstanceID()))
-			affecting.Remove(ai.gameObject.GetInstanceID());
-		ai.OnEvaded(gameObject);
-	}
-}
-
-function OnDestroy(){
-	for (var a:Object in affecting.Values){
-		Debug.Log('gravity dodger dead!');
-		(a as DodgingAI).OnEvaded(gameObject);
+		ai.OnEvadeZone(dir.normalized*(affectRadius-dir.magnitude));
 	}
 }

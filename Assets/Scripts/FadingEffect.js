f9be08c849779204ae05a81da5952866f286f086
no7boy <no7boy@gmail.com>
2011-12-05 10:24:46 +0000

    fading to enemy

--- Assets/Scripts/FadingEffect.js
+++ Assets/Scripts/FadingEffect.js
@@ -1,4 +1,8 @@
-public var fadingTime : float = 2.0; // in seconds
+#pragma strict
+
+
+public var fadingTime : float = 1.0; // in seconds
+//public var PairMaterial ;
 public var opaqueShader : Shader;
 public var transparentShader : Shader;
 
@@ -13,6 +17,8 @@ private var _renderer : SkinnedMeshRenderer;
 function Awake() {
 	_renderer = gameObject.GetComponentInChildren.<SkinnedMeshRenderer>();
 	enabled = false;
+	opaqueShader = Shader.Find("FateHunter/Normal-Character");
+	transparentShader = Shader.Find("FateHunter/Transparent-Character");
 	Reset();
 }
 
@@ -33,24 +39,32 @@ function OnEnable() {
 	_renderer.material.shader = transparentShader;
 }
 
+function OnDisable(){
+	if (_fadingState == FadingState.In)
+	{
+		_renderer.material.shader = opaqueShader;
+		_color = _colorStart;
+	}
+}
+
 function Reset() {
 	_color.a = 255;
 	_renderer.material.shader = opaqueShader;
-	fadingState = FadingState.None;
+	_fadingState = FadingState.None;
 }
 
 function Update () {
 	_elapsedTime -= Time.deltaTime;
-
+	Debug.Log("Alpha" + _color.a);
 	if ( _elapsedTime < 0) {
 		enabled = false;
 		return;
 	}
 	
 	if (_fadingState == FadingState.Out){
-		_color.a =  ( _elapsedTime / fadingTime);
+		_color.a =  255*( _elapsedTime / fadingTime);
 	} else {
-		_color.a = 1.0 -  ( _elapsedTime / fadingTime);
+		_color.a = 255*(1.0 -  ( _elapsedTime / fadingTime));
 	}
 	
 	_renderer.material.SetColor("_Color",_color);
--- Assets/Scripts/VisibleBucket.js
+++ Assets/Scripts/VisibleBucket.js
@@ -1,14 +1,25 @@
 #pragma strict
+@script RequireComponent(FadingEffect)
 public var visibleObjects:GameObject[];
 public var recursive:boolean = false;
 
 function OnSetVisible(visi:boolean){
+	Debug.Log("Enemy set visible:" + visi);
 	for (var obj:GameObject in visibleObjects){
 		if (recursive){
 			var rr : Renderer[] = obj.GetComponentsInChildren.<Renderer>();
 			for (var r:Renderer in rr)
 				r.enabled = visi;
-		}else
+		}else {
 			obj.GetComponent.<Renderer>().enabled = visi;	
+		}
 	}
+	if (!visi)
+	{
+		gameObject.GetComponent(FadingEffect).FadeOut();
+	} else
+	{
+		gameObject.GetComponent(FadingEffect).FadeIn();
+	}
+	
 }

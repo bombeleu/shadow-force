3da8f2f3402615dde69a132c65ebe10d0956c8ba
hnam1985 <hnam1985@gmail.com>
2011-12-09 04:46:11 +0000

    add persistent data storage, better menu add LevelInfo.js

---
+++ Assets/Scripts/GUI/LevelInfo.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+public var weapon0 : Weapon;
+public var weapon1 : Weapon;
+
+function Awake(){//this function is called when the level is loaded, always after SelectWeaponGUI is initialized
+	var selector:SelectWeaponGUI = GameObject.FindObjectOfType(SelectWeaponGUI);
+	for (var i:int = 0; i < selector.availableWeapons.Length; i++){
+		var wp:Weapon = selector.availableWeapons[i];
+		if (wp==weapon0) selector.selectedWeapons[0] = i;
+		if (wp==weapon1) selector.selectedWeapons[1] = i;
+	}
+	Debug.Log("level info finished");
+	
+	selector.SendMessage("OnNetworkLoadedLevel",	SendMessageOptions.DontRequireReceiver);//cause selector and instantiate are ont the same obj
+}

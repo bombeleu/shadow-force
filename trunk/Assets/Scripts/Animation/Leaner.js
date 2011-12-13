4808457bfa7db6ede7a599738e6905fef79e395e
hnam1985@gmail.com <hnam1985@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2011-12-13 17:48:44 +0000

    lean to correct side

--- trunk/Assets/Scripts/Animation/Leaner.js
+++ trunk/Assets/Scripts/Animation/Leaner.js
@@ -11,6 +11,7 @@ function Awake(){
 
 private var leaning:boolean = false;
 private var leanNormal:Vector3;
+private var leanRight:boolean;
 
 function check(collision : Collision){
 	if (leaning) return;
@@ -19,14 +20,18 @@ function check(collision : Collision){
     	if (box && box.gameObject.CompareTag("Blocker")){
     		var localP : Vector3 = box.transform.worldToLocalMatrix.MultiplyPoint(contact.point);
     		localP.Scale(box.transform.localScale);
+    		var sign:float = localP.x*localP.z;
     		localP.x = Mathf.Abs(localP.x);
     		localP.z = Mathf.Abs(localP.z);
     		var size : Vector3 = Vector3.Scale(box.size,box.transform.localScale) * 0.5 - 1.25*radius;
     		var downSize : Vector3 = size - radius;
     		/*Debug.Log(localP);
     		Debug.Log(size);*/
-    		if ((localP.x > downSize.x && localP.x < size.x)
-    			|| (localP.z > downSize.z && localP.z < size.z)){
+    		var x_axis:boolean = (localP.x > downSize.x && localP.x < size.x);
+    		var z_axis:boolean = (localP.z > downSize.z && localP.z < size.z);
+    		
+    		if (x_axis || z_axis){
+    			leanRight = x_axis?sign > 0:sign < 0;
     			leanNormal = contact.normal;
     			leaning = true;
     			break;
@@ -45,7 +50,7 @@ function OnCollisionStay(collision : Collision) {
 
 function FixedUpdate(){
 	if (leaning)
-		playerAnimation.Lean(leanNormal);
+		playerAnimation.Lean(leanNormal, leanRight);
 	else
 		playerAnimation.ExitLean();
 	leaning = false;

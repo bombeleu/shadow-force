f9c70b1b73eb81cca9f8a5cf8c446142dd8ec8c7
no7boy@gmail.com <no7boy@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2011-12-14 13:38:01 +0000

    fixed ragdoll

--- trunk/Assets/Scripts/RagdollController.js
+++ trunk/Assets/Scripts/RagdollController.js
@@ -12,6 +12,19 @@ function Start()
 	if (debug) Debug.Log("Ragdoll controller started");
 	ResetAnimation();
 }
+private function RecursiveIgnoreRagdollCollision(trans : Transform)
+{
+	if (trans.collider != null) 
+	{
+		Physics.IgnoreCollision(trans.collider,gameObject.collider);
+		//Debug.Log(trans.collider);
+	}
+	for(var t : Transform in trans)
+	{
+		RecursiveIgnoreRagdollCollision (t);
+	}
+}
+
 
 function Update () 
 {
@@ -33,7 +46,7 @@ function Update ()
 	}
 }
 
-function RecursiveDeepActivation(trans : Transform)
+private function RecursiveDeepActivation(trans : Transform)
 {
 	trans.gameObject.active = true;
 	for(var t : Transform in trans)
@@ -52,11 +65,41 @@ public function ResetAnimation()
 	// enable animation
 	animationObject.GetComponentInChildren.<SkinnedMeshRenderer>().enabled = true;
 	animationObject.animation.enabled = true;
+
+	// enabled collider in gameobject
+	gameObject.collider.enabled = true;
 	
 	// enable visiont mesh
 	gameObject.GetComponentInChildren.<VisionMeshScript>().TurnOn();
 }
 
+private function CopyTransforms(src : Transform, dest : Transform)
+{
+    // error checking
+    if (!src || !dest)
+    {
+    	Debug.Log("Copy transform, parameter is NULL");
+        return;
+    }
+
+    // copy position
+    dest.localPosition = src.localPosition;
+    // copy rotation
+    dest.localRotation = src.localRotation;
+    // copy scale
+    dest.localScale = src.localScale;
+
+	//Debug.Log(src);
+    // copy children
+    for(var sc : Transform in src)
+    {
+        var dc : Transform = dest.Find(sc.name);
+
+        if (dc)
+            CopyTransforms(sc, dc);
+    }
+}
+    
 public function Die()
 {
 	if (debug) Debug.Log("Ragdoll die activated");
@@ -65,12 +108,19 @@ public function Die()
 	animationObject.animation.Stop();
 	animationObject.animation.enabled = false;
 	animationObject.GetComponentInChildren.<SkinnedMeshRenderer>().enabled = false;
+
 	// disabled vision mesh
 	gameObject.GetComponentInChildren.<VisionMeshScript>().TurnOff();
-	
+		
 	// then activate ragdoll
 	RecursiveDeepActivation(ragDoll.transform);
 	ragDoll.GetComponentInChildren.<SkinnedMeshRenderer>().enabled = true;
+	
+	RecursiveIgnoreRagdollCollision(ragDoll.transform);
+	
+	// copy transforms
+    CopyTransforms(animationObject.transform,ragDoll.transform);
+    activated = true;
 }
 
 public function DieByForce(force : Vector3)

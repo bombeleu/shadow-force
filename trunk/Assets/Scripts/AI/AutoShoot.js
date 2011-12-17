0e5571975b1f1f863fb57e54c44d09bf4c01c734
hnam1985@gmail.com <hnam1985@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2011-12-17 17:49:30 +0000

    fix autoshoot bug on stickyCam disable control after dead

--- trunk/Assets/Scripts/AI/AutoShoot.js
+++ trunk/Assets/Scripts/AI/AutoShoot.js
@@ -54,11 +54,12 @@ function Update(){
 	}else{
 		firing = true;
 		var tarPos:Vector3;
-		tarPos.y = transform.position.y;
 		for (var i:Object in enemies.Values){
 			tarPos = (i as Visibility).transform.position;
 			break;
 		}
+		tarPos.y = transform.position.y;
+		//Debug.Log("target found:"+tarPos);
 		var targetV:Vector3 = tarPos - transform.position;
 		//motor.movementDirection = Vector3.zero;//targetV;
 		var weapon:Weapon = weaponManager.GetCurrentWeapon();

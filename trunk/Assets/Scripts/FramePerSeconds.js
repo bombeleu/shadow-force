3ba76fb4e46c4f9f872fa3d2cf80d4e5bb54a294
no7boy@gmail.com <no7boy@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2011-12-28 03:00:09 +0000

    fix fps on flash

--- trunk/Assets/Scripts/FramePerSeconds.js
+++ trunk/Assets/Scripts/FramePerSeconds.js
@@ -1,8 +1,8 @@
 
 var updateInterval = 0.5;
 
-private var accum = 0.0; // FPS accumulated over the interval
-private var frames = 0; // Frames drawn over the interval
+private var accum : float = 0.0; // FPS accumulated over the interval
+private var frames : int = 0; // Frames drawn over the interval
 private var timeleft : float; // Left time for current interval
 
 function Start()
@@ -26,7 +26,11 @@ function Update()
     if( timeleft <= 0.0 )
     {
         // display two fractional digits (f2 format)
-        guiText.text = "" + (accum/frames).ToString("f2");
+        //guiText.text = "" + (accum/frames).ToString("f2");
+        var ffps : float = accum/frames;
+        var ifps : int = ffps;
+        guiText.text = "FPS:" + ifps.ToString();
+        
         timeleft = updateInterval;
         accum = 0.0;
         frames = 0;

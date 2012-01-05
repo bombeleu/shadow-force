9c9187fe5d0409a30dafd2d86d1f13a4b3fad076
hnam1985@gmail.com <hnam1985@gmail.com@855abfd9-2dab-a669-9c2c-a9d809d4b390>
2012-01-05 11:57:40 +0000

    New_Level_shotgun_stickyCam.unity EnemyLizard.prefab back to use rigidbody to avoid shaking dodging

--- trunk/Assets/Scripts/AI/SphericalDodger.js
+++ trunk/Assets/Scripts/AI/SphericalDodger.js
@@ -14,11 +14,11 @@ function OnTriggerStay (other : Collider) : void{
 		dir.y = 0;
 		var affectDist:float = sCollider.radius - DodgingAI.dodgingBuffer + DodgingAI.dodgerRadius;
 		var dist:float = dir.magnitude;
-		//ai.OnEvadeZone( dist<=affectDist? 
-		//	dir.normalized*(affectDist - dist):
-		//	Vector3.zero, true);//stay in buffer zone
-		ai.OnEvadeZone(  
-			dir.normalized*(dist<=affectDist?(affectDist - dist):
-			FreeMovementMotor.ControllerOffset), true);//stay in buffer zone
+		ai.OnEvadeZone( dist<=affectDist? 
+			dir.normalized*(affectDist - dist):
+			Vector3.zero, true);//stay in buffer zone
+		//ai.OnEvadeZone(  
+		//	dir.normalized*(dist<=affectDist?(affectDist - dist):
+		//	FreeMovementMotor.ControllerOffset), true);//stay in buffer zone
 	}
 }
